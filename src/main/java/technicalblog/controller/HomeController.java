4d5e7d0761448efa207eeece5288cbe7b194cb39
harshkumar0512 <55206284+harshkumar0512@users.noreply.github.com>
2020-10-24 09:37:28 +0530

    implemented user login and logout functionality

--- src/main/java/technicalblog/controller/HomeController.java
+++ src/main/java/technicalblog/controller/HomeController.java
@@ -1,40 +1,32 @@
 package technicalblog.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import technicalblog.model.Post;
+import technicalblog.service.PostService;
 
 import java.util.ArrayList;
 import java.util.Date;
 
 @Controller
 public class HomeController {
+
+    public HomeController(){
+        System.out.println("*** Home Controller ***");
+    }
+
+    @Autowired
+    private PostService postService;
+
     @RequestMapping("/")
     public String getAllPost(Model model){
 
-        ArrayList<Post> posts = new ArrayList<Post>();
-
-        Post post1 = new Post();
-         post1.setTitle("Post 1");
-         post1.setBody("Post body 1");
-         post1.setDate(new Date());
-
-        Post post2 = new Post();
-        post2.setTitle("Post 2");
-        post2.setBody("Post body 2");
-        post2.setDate(new Date());
-
-        Post post3 = new Post();
-        post3.setTitle("Post 3");
-        post3.setBody("Post body 3");
-        post3.setDate(new Date());
-
-        posts.add(post1);
-        posts.add(post2);
-        posts.add(post3);
+        ArrayList<Post> posts = postService.getAllPosts();
 
         model.addAttribute("posts",posts);
+
         return "indexTechnical";
     }
 }
---
+++ src/main/java/technicalblog/controller/PostController.java
@@ -0,0 +1,23 @@
+package technicalblog.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import technicalblog.model.Post;
+import technicalblog.service.PostService;
+
+import java.util.ArrayList;
+
+@Controller
+public class PostController {
+    @Autowired
+    PostService postService;
+
+    @RequestMapping("posts")
+    public String getUserPosts(Model model){
+        ArrayList<Post> posts = postService.getOnePosts();
+        model.addAttribute("posts", posts);
+        return "posts";
+    }
+}
